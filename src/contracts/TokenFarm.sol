77bc416dd30e84379b19e3fe9aad4e0960d2a61c
IMPWONG <matisse.acheen.pro@gmail.com>
2021-02-16 15:10:06 -0500

    Issue Token

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -12,9 +12,36 @@ contract TokenFarm {
   DappToken public dappToken;
   DaiToken public daiToken;
 
+  address[] public stackers;
+  mapping(address => uint) public stackingBalance;
+  mapping(address => bool) public hasStacked;
+  mapping(address => bool) public isStacking;
+
   constructor(DappToken _dappToken, DaiToken _daiToken) public {
     dappToken = _dappToken;
     daiToken = _daiToken;
   }
+
+  //1. Stack Tokens (Deposit)
+    function stakeTokens(uint _amount) public{
+      //Code goes here
+        // Transfer moke dai token to this contract for stacking
+          daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        //Update Stacking Balance
+          stackingBalance[msg.sender] = stackingBalance[msg.sender] + _amount;
+
+        //Add user to stackers array only if they haven't stacked already
+        if(!hasStacked[msg.sender]){
+          stackers.push(msg.sender);
+        }
+
+        //Updating stacking status
+        isStacking[msg.sender] = true;
+        hasStacked[msg.sender] = true;
+
+}
+  //2. Unstack Tokens (Withdraw)
+  //3. Issuing Tokens
 
 }
