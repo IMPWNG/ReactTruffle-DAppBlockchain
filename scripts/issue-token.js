1686c5d01e81776d4535940c1b66e6b2863d601d
IMPWONG <matisse.acheen.pro@gmail.com>
2021-02-24 15:27:07 -0500

    Script done

--- scripts/issue-token.js
+++ scripts/issue-token.js
@@ -1,6 +1,8 @@
 const TokenFarm = artifacts.require("TokenFarm");
 
 module.exports = async function(callback) {
+  let tokenFarm = await TokenFarm.deployed()
+  await tokenFarm.issueTokens()
   console.log("Tokens Issued!")
 
   callback()
